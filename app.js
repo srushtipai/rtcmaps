8c107be08a5eed5b8b8f32e88afeb6f7857ccfcb
Hamza Nasir <mnasir@hawk.iit.edu>
2018-04-10 21:20:46 +0000

    Initial structure of basic express application

---
+++ app.js
@@ -0,0 +1,33 @@
+require('dotenv').load();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const flash = require('connect-flash');
+
+const app = express();
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 120000 },
+}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(flash());
+app.set('view engine', 'ejs');
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash('error');
+  res.locals.success = req.flash('success');
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Inital Check');
+});
+
+app.listen(process.env.PORT, process.env.IP, () => {
+  console.log('RTC map server starting.');
+});
