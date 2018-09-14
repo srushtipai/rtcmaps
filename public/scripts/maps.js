cb0cd0571b96e65e86a68e9549b9ecd396621667
Srushti Pai <srushtipai17@gmail.com>
2018-09-14 01:28:03 -0500

    Form and show beacons functionailty

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -23,30 +23,11 @@ $(document).ready(function() {
 
   const mobile = $(window).width() <= 500;
   renderSVG(mobile, $('#floor').select2('data')[0].text, true);
-  //renderSVG(mobile, $('#building').select2('data')[0].text, true);
+
   //
-  /*
-  $('#openMaps').click(() => {
-    let b = $('#building').find(':selected').text();
-
-    if (b === 'Stuart') {
-      window.location.href = '/stuart';
-    } else if (b === 'Alumini') {
-      window.location.href = '/alumini';
-    }
-  });*/
-
-
-
-  // Define type of device
-  var device;
-  var current_bulding;
-  var current_floor;
-  var current_location = $('#floor').select2('data')[0].text;
-  //
-  $('#toggle-2').change(function () {
+  $('#showBeacons').change(function () {
     if ($(this).is(':checked')) {
-      renderMockBeacons(mobile);
+      renderBeacons(mobile);
     } else {
       d3.selectAll('circle').remove();
     }
@@ -54,70 +35,42 @@ $(document).ready(function() {
 
   $('#addmode').change(function () {
     $('#addgateway').prop('checked', false);
-    var temp_location = current_location;
     if ($('#addmode').prop('checked') == true) {
-        d3.select('svg').on("click", function () {
+      d3.select('svg').on("click", function () {
         // This function will run when someone clicks on map when add mode is activated
-        device ="Beacon";
-        let coordinates = d3.mouse(this);
-        var set = deviceRegister(device);
-        if (set == true){
+          let coordinates = d3.mouse(this);
           let position = realPosition(coordinates[0], coordinates[1], mobile);
-          renderBeacon(coordinates[0], coordinates[1], position.x, position.y, temp_location);
-        }
+          renderBeacon(coordinates[0], coordinates[1], position.x, position.y);
+          $('#beaconForm').modal('show');
+          $('#beaconForm #xValue').val(position.x);
+          $('#beaconForm #yValue').val(position.y);
       });
     } else {
-      d3.selectAll('circle').remove();
+      d3.select('svg').on('click', null);
     }
   });
 
   $('#addgateway').change(function () {
     $('#addmode').prop('checked', false);
-    var temp_location = current_location;
-    if ($('#addgateway').is(':checked')) {
-      //alert(current_location);
+    if ($('#addgateway').prop('checked') == true) {
       d3.select('svg').on("click", function () {
-        //alert("Add GateWay");
         // This function will run when someone clicks on map when add mode is activated
-        device = "Gateway";
         let coordinates = d3.mouse(this);
-        var set = deviceRegister(device);
-        if (set == true){
-          let position = realPosition(coordinates[0], coordinates[1], mobile);
-          renderGateway(coordinates[0], coordinates[1], position.x, position.y, temp_location);
-        }
+        let position = realPosition(coordinates[0], coordinates[1], mobile);
+        renderGateway(coordinates[0], coordinates[1], position.x, position.y);
+        $('#gatewayForm').modal('show');
+        $('#gatewayForm #xValue').val(position.x);
+        $('#gatewayForm #yValue').val(position.y);
       });
-    }
-  });
-
-  $('#editDevice').change(function () {
-    $('#addmode').prop('checked', false);
-    $('#addgateway').prop('checked', false);
-    var temp_location = current_location;
-    if ($('#editDevice').is(':checked')) {
-      //alert(current_location);
-      d3.select('svg').on("click", function () {
-
-        //alert("Add GateWay");
-        // This function will run when someone clicks on map when add mode is activated
-        /*
-        device = "Gateway";
-        let coordinates = d3.mouse(this);
-        var set = deviceRegister(device);
-        if (set == true){
-          let position = realPosition(coordinates[0], coordinates[1], mobile);
-          renderGateway(coordinates[0], coordinates[1], position.x, position.y, temp_location);
-        }*/
-      });
+    } else {
+        d3.select('svg').on('click', null);
     }
   });
 
   $('#floor').on('select2:select', function (e) {
     $('#addmode').prop('checked', false);
     $('#addgateway').prop('checked', false);
-    alert($('#addgateway').val());
     var data = e.params.data;
-    current_location = data.text;
     renderSVG(mobile, data.text, false);
   });
 
@@ -134,27 +87,6 @@ $(document).ready(function() {
     renderSVG(mobile, data2.text, false);
   });
 });
-//Register Pop up
-var device_ID;
-var Room_ID;
-function deviceRegister(device) {
-    var set;
-    device_ID = prompt("Please enter "+ device + " MAC address:", "");
-    Room_ID = prompt("Please enter room number or location:", "");
-    if (device_ID == null || device_ID == "" || Room_ID == null || Room_ID =="" ) {
-        alert("Missing field(s) or User cancelled the prompt");
-        set = false;
-    } else {
-        alert(""+ device +" MAC: " + device_ID + " registered");
-        set = true;
-    }
-    return set;
-    //document.getElementById("demo").innerHTML = txt;
-}
-
-
-
-
 //returns real life x and y from locked origin in meters
 
 function realPosition(svgX, svgY, mobile) {
@@ -181,39 +113,32 @@ function getRandomNumber(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-function renderMockBeacons(mobile) {
-  const viewBox = d3.select('svg').attr('viewBox').split(" ");
-  const width = parseInt(viewBox[2], 10) - 200;
-  const height = parseInt(viewBox[3], 10) - 200;
-  let i;
-  let x;
-  let y;
-  let tempPosition;
-  for (i = 0; i < 10; i++) {
-    x = getRandomNumber(200, width);
-    y = getRandomNumber(100, height);
-    tempPosition = realPosition(x, y, mobile);
-    renderBeacon(getRandomNumber(200, width), getRandomNumber(100, height), tempPosition.x, tempPosition.y);
-  }
-  const dragHandler = d3.drag()
-      .on("drag", function () {
-        d3.select(this)
-            .selectAll('circle')
-            .attr("cx", d3.event.x)
-            .attr("cy", d3.event.y);
-      })
-      .on('end', function() {
-        // this function runs after the user drops the beacon to its new position
-          const position = realPosition(d3.event.x, d3.event.y, mobile);
-          d3.select(this).select('.mainCircle')
-              .attr('fill', 'red')
-              .attr('data-original-title', `x: ${Number((position.x).toFixed(2))} y: ${Number((position.y).toFixed(2))}`);
-      });
+function renderBeacons(mobile) {
+  $.get('https://api.iitrtclab.com/beacons/AM/1', (beacons) => {
+    beacons.forEach((beacon) => {
+      setBeacon(beacon.x, beacon.y, mobile);
+    });
 
-  dragHandler(d3.selectAll(".beacons"));
+    const dragHandler = d3.drag()
+        .on("drag", function () {
+          d3.select(this)
+              .selectAll('circle')
+              .attr("cx", d3.event.x)
+              .attr("cy", d3.event.y);
+          })
+        .on('end', function() {
+          // this function runs after the user drops the beacon to its new position
+            const position = realPosition(d3.event.x, d3.event.y, mobile);
+            d3.select(this).select('.mainCircle')
+                .attr('fill', 'red')
+                .attr('data-original-title', `x: ${Number((position.x).toFixed(2))} y: ${Number((position.y).toFixed(2))}`);
+          });
+
+      dragHandler(d3.selectAll(".beacons"));
+  });
 }
 
-function renderBeacon (x, y, realX, realY, temp_location) {
+function renderBeacon (x, y, realX, realY) {
 
   var group = d3.select('svg').append('g').attr('class', 'beacons');
 
@@ -221,10 +146,7 @@ function renderBeacon (x, y, realX, realY, temp_location) {
   group.append('circle')
           .attr("cx", x)
           .attr("cy", y)
-          .attr("r", 15)
-          .on('click',function(){
-            Room_ID = prompt("Please enter new room number or location:", "");
-          });
+          .attr("r", 15);
 
   group.append('circle')
           .attr('class', 'mainCircle')
@@ -232,7 +154,7 @@ function renderBeacon (x, y, realX, realY, temp_location) {
           .attr("cy", y)
           .attr("r", 0)
           .attr("data-toggle", "tooltip")
-          .attr("title", `x: ${Number((realX).toFixed(2))} y: ${Number((realY).toFixed(2))} Device Type: Becon Becon_ID: ${device_ID} Bulding: ${temp_location} Room_ID: ${Room_ID}`)
+          .attr("title", `x: ${Number((realX).toFixed(2))} y: ${Number((realY).toFixed(2))}`)
           .on('mouseover', function() {
             d3.select(this).transition()
                            .duration(300)
@@ -247,10 +169,6 @@ function renderBeacon (x, y, realX, realY, temp_location) {
                            .duration(300)
                            .attr("r", "50");
           })
-          .on('click',function(){
-            //alert("click");
-            Room_ID = prompt("Please enter new room number or location:", "");
-          })
           .style("fill", 'rgb(88, 91, 96)')
           .style("fill-opacity", "0.6")
           .style("stroke", "black")
@@ -260,30 +178,9 @@ function renderBeacon (x, y, realX, realY, temp_location) {
           .duration(300)
           .attr("r", 50)
           .attr("transform", "rotate(180deg)")
-
-
-  //drag
-
-  const dragHandler = d3.drag()
-      .on("drag", function () {
-        d3.select(this)
-            .selectAll('circle')
-            .attr("cx", d3.event.x)
-            .attr("cy", d3.event.y);
-      })
-      .on('end', function() {
-        // this function runs after the user drops the beacon to its new position
-          const position = realPosition(d3.event.x, d3.event.y, mobile);
-          d3.select(this).select('.mainCircle')
-              .attr('fill', 'red')
-              .attr('data-original-title', `x: ${Number((position.x).toFixed(2))} y: ${Number((position.y).toFixed(2))}`);
-      });
-
-  dragHandler(d3.selectAll(".beacons"));
-
 }
 
-  function renderGateway (x, y, realX, realY, temp_location) {
+  function renderGateway (x, y, realX, realY) {
     var group = d3.select('svg').append('g').attr('class', 'beacons');
 
     group.append('circle')
@@ -291,15 +188,13 @@ function renderBeacon (x, y, realX, realY, temp_location) {
             .attr("cy", y)
             .attr("r", 15);
 
-
-
     group.append('circle')
             .attr('class', 'mainCircle')
             .attr("cx", x)
             .attr("cy", y)
             .attr("r", 0)
             .attr("data-toggle", "tooltip")
-            .attr("title", `x: ${Number((realX).toFixed(2))} y: ${Number((realY).toFixed(2))} Device Type: Gateway Gateway_ID: ${device_ID} Bulding: ${temp_location} Room_ID: ${Room_ID}`)
+            .attr("title", `x: ${Number((realX).toFixed(2))} y: ${Number((realY).toFixed(2))}`)
 
             .on('mouseover', function() {
               d3.select(this).transition()
@@ -308,16 +203,12 @@ function renderBeacon (x, y, realX, realY, temp_location) {
 
               $(this).tooltip();
               $(this).tooltip('show');
-              //alert("Hello");
             })
 
             .on('mouseout', function () {
               d3.select(this).transition()
                              .duration(300)
                              .attr("r", "50");
-            })
-            .on('click',function(){
-
             })
             .style("fill", 'rgb(255, 0, 0)')
             .style("fill-opacity", "0.6")
@@ -429,9 +320,9 @@ function inverseMapY(svgY) {
 
 function setBeacon(x,y, mobile) {
   if (mobile) {
-    setLocation(mapX(x), mapY(y), 100);
+    renderBeacon(mapX(x), mapY(y), x, y);
   } else {
     const newX = mapX(parseFloat(d3.select('svg').attr('data-width'), 10)) - mapX(x);
-    setLocation(mapY(y), newX, 100);
+    renderBeacon(mapY(y), newX, x, y);
   }
 }
